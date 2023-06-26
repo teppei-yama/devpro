754c086acd00b75df2c7e23fa8eab7653bf0fce4
naoyahorai <0911horai@gmail.com>
2023-06-26 13:53:39 +0900

    add flask code

--- server/main.py
+++ server/main.py
@@ -1,14 +1,27 @@
-import data_server_1_PC 
+import data_server_1_PC
 import sys
 import time
 import json
 import csv
 import datetime
+from flask import Flask
+
+app = Flask(__name__)
 
 SERVER = 'localhost'
 WAITING_PORT = 8765
 
 LOOP_WAIT = 3
+
+count = 0
+
+@app.route("/hello",methods=["GET"])
+def hello_world():
+    return "<a href='../'>return top</a>"
+
+@app.route("/",methods=["GET"])
+def top_page():
+    return f"<a href='/hello'>go to hello</a><p>you visit hello {count} times</p>"
 
 
 if __name__ == '__main__':
@@ -35,4 +48,5 @@ if __name__ == '__main__':
 
             count = count + 1
 
-    data_server_1_PC.server(hostname_v, waiting_port_v)
+    #data_server_1_PC.server(hostname_v, waiting_port_v)
+    app.run(host = '0.0.0.0', port = 5001 ,debug = True)
