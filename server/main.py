585834a445a4266929fa20fb03919858e8aabfcd
iput2023-oikawa <tk210503ryo.oikawa@gmail.com>
2023-07-24 14:40:11 +0900

    湿度追加機能 and csvを読み込む and 不適切な入力に対するエラー処理

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-import data_server_PC
+from data_server_PC import csv_read_iterator
 import sys
 import time
 import json
@@ -15,37 +15,51 @@ LOOP_WAIT = 3
 
 count = 0
 
-data_list = [
-    [35.2, 43.0],
-    [27.5, 32.0],
-    [24.0, 36.0],
-    [18.0, 5.0]
-]
+# data_list = [
+#     [35.2, 43.0],
+#     [27.5, 32.0],
+#     [24.0, 36.0],
+#     [18.0, 5.0]
+# ]
+filename = "./server/csv/csv_datalist.csv"
+#filename = 'C:/Users/210503oikawa/Downloads/IoTデバイスプログラミングⅢ/devpro3_G1_sensor/server/csv/csv_datalist.csv'
+data_list = csv_read_iterator(filename)
 
 @app.route("/",methods=["GET"])
 def top_page():
-    # data_list = []
-    # data_list.append([35.2, 43.0])
-    # data_list.append([27.5, 32.0])
-    # data_list.append([24.0, 36.0])
-    # data_list.append([18.0, 5.0])
+    
     return render_template('index.html', input_from_python = data_list)
 
 @app.route("/", methods=["POST"])
 def top_page2():
-     print("Hello! (to Terminal) POST")
-     text_from_html = request.form["new_tempe"]
-     print(text_from_html)
-     add_tempe = float(text_from_html)
-     add_humid = 0
-     #data_list = []
-    #  data_list.append([35.2, 43.0])
-    #  data_list.append([27.5, 32.0])
-    #  data_list.append([24.0, 36.0])
-    #  data_list.append([18.0, 5.0])
-    #  data_list.append([add_tempe, add_humid])
-     data_list.append([add_tempe, add_humid])
-     return render_template("index.html",input_from_python = data_list)
+    print("Hello! (to Terminal) POST")
+    text_from_html_tempe = request.form["new_tempe"]
+    text_from_html_humid = request.form["new_humid"]
+    print(text_from_html_tempe)
+    print(text_from_html_humid)
+    try:
+        add_tempe = float(text_from_html_tempe)
+        if add_tempe < 0 or add_tempe > 50:
+            raise ValueError("Temperature value must be between 0 and 50. Please enter a valid numeric value.")
+    except ValueError as e:
+        if "could not convert string to float" in str(e):
+            error_message = "Invalid temperature value. Please enter a valid numeric value."
+        else:
+            error_message = str(e)
+        return render_template("error.html", error_message=error_message)
+    try:
+        add_humid = float(text_from_html_humid)
+        if add_humid < 0 or add_humid > 100:
+            raise ValueError("Humidity value must be between 0 and 100. Please enter a valid numeric value.")
+    except ValueError as e:
+        if "could not convert string to float" in str(e):
+            error_message = "Invalid humidity value. Please enter a valid numeric value."
+        else:
+            error_message = str(e)
+        return render_template("error.html", error_message=error_message)
+    #add_humid = 0
+    data_list.append([add_tempe, add_humid])
+    return render_template("index.html",input_from_python = data_list)
 
 
 if __name__ == '__main__':
