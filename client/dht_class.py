4663f7b07cd25ff3dac940fd2634507e59c6f3a1
naoyahorai <0911horai@gmail.com>
2023-07-29 04:41:47 +0900

    moromoro

--- client/dht_class.py
+++ client/dht_class.py
@@ -1,31 +1,31 @@
-import adafruit_dht
-from board import *
+#import adafruit_dht
+#from board import *
 import time
 import datetime
 
 class DHT11:
-    def __init__(self, pin=D4):
-        self.instance_dht = adafruit_dht.DHT11(pin=pin, use_pulseio=False)
+    def __init__(self):
+        #self.instance_dht = adafruit_dht.DHT11(pin=pin, use_pulseio=False)
         self.WAIT_INTERVAL = 10
         self.WAIT_INTERVAL_RETRY = 10
 
-    def get_dht_data(self):
-        temp_dht = 200.0  # unnecessary value-setting
-        humid_dht = 100.0  # unnecessary value-setting
-        try:
-            self.instance_dht.measure()
-            temp_dht = self.instance_dht.temperature
-            humid_dht = self.instance_dht.humidity
+    #def get_dht_data(self):
+    #    temp_dht = 200.0  # unnecessary value-setting
+    #    humid_dht = 100.0  # unnecessary value-setting
+    #    try:
+    #        self.instance_dht.measure()
+    #        temp_dht = self.instance_dht.temperature
+    #        humid_dht = self.instance_dht.humidity
 
-        except RuntimeError:
-            print("RuntimeError: DHT11/22 returns wrong values, maybe.: " + str(datetime.datetime.now()))
-            raise RuntimeError
+    #    except RuntimeError:
+    #        print("RuntimeError: DHT11/22 returns wrong values, maybe.: " + str(datetime.datetime.now()))
+    #        raise RuntimeError
 
-        except OSError:
-            print("OSError: DHT11/22: OS Error, but we ignore it.: " + str(datetime.datetime.now()))
-            raise OSError
+    #    except OSError:
+    #       print("OSError: DHT11/22: OS Error, but we ignore it.: " + str(datetime.datetime.now()))
+    #        raise OSError
 
-        return float(temp_dht), float(humid_dht)
+    #    return float(temp_dht), float(humid_dht)
 
     def get_dht_data_for_json(self):
         tempe = 40.0
@@ -35,7 +35,7 @@ class DHT11:
 
         while True:
             try:
-                tempe, humid = self.get_dht_data()
+                tempe, humid = 50,50
                 now_str = str(datetime.datetime.now())
                 print("Temperature: %f  Humidity: %f" % (tempe, humid), now_str)            
                 data_list.append({'temperature': tempe, 'humidity': humid})
